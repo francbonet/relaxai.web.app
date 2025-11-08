9b626c9ce90b59b07d7dbd7416a18ab2747c2699
Francesc Perez <francbonet@gmail.com>
2025-11-08 02:07:04 +0100

    deploy: GitHub Pages

--- appBundle.js
+++ appBundle.js
@@ -3,7 +3,7 @@
  * SDK version: 5.5.5
  * CLI version: 2.14.2
  *
- * Generated: Sat, 08 Nov 2025 01:04:32 GMT
+ * Generated: Sat, 08 Nov 2025 01:07:00 GMT
  */
 
 var APP_com_domain_app_demov2 = (function () {
